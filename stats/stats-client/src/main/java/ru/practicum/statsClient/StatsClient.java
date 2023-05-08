507656b067ccca6fc049ef1ae485e27bbb0de727
Dehuckoo1oo <dehuckoo1oo@mail.ru>
2023-05-08 16:23:13 +0300

    Исправить code-style

--- stats/stats-client/src/main/java/ru/practicum/statsClient/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/statsClient/StatsClient.java
@@ -23,7 +23,7 @@ import java.util.stream.Collectors;
 public class StatsClient extends BaseClient {
 
     @Autowired
-    public StatsClient(@Value("http://localhost:9090") String serverUrl, RestTemplateBuilder builder) {
+    public StatsClient(@Value("${stats-server.uri}") String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder
                         .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
